62b10eebeb43b96f3549992ffa8fddc4ea948e69
shorty <dudeshorty0@gmail.com>
2025-12-23 23:52:46 +0530

    fix an issue

--- src/main/java/com/research/microservice_experiment/model/User.java
+++ src/main/java/com/research/microservice_experiment/model/User.java
@@ -1,13 +1,18 @@
 package com.research.microservice_experiment.model;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@Entity
+@Table(name = "users")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String username;
     private String email;
--- src/main/java/com/research/microservice_experiment/repository/UserRepository.java
+++ src/main/java/com/research/microservice_experiment/repository/UserRepository.java
@@ -1,12 +1,11 @@
 package com.research.microservice_experiment.repository;
 
 import com.research.microservice_experiment.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import java.util.Optional;
 
 @Repository
-public interface UserRepository {
+public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
-    User save(User user);
 }
